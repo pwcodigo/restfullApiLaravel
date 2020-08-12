1e19a8f0bfae308eeb3e5412d4d1965e4af952d8
pwcodigo <pwcodigo@gmail.com>
2020-08-12 18:23:55 -0300

    feat: Criando seed no banco de dados, criado ProductController

--- app/database/factories/ProductFactory.php
+++ app/database/factories/ProductFactory.php
@@ -1,12 +1,18 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Model;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'price' => $faker->randomFloat(2,0,8),
+        'description' => $faker->text
+
+
+
     ];
 });
