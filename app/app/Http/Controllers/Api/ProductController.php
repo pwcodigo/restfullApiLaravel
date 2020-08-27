dffe93cc91ca91571db74a07d1d3de4fa6a671fd
pwcodigo <pwcodigo@gmail.com>
2020-08-27 04:03:00 -0300

    feat: Funcao de atualizar via API está funcionando

--- app/app/Http/Controllers/Api/ProductController.php
+++ app/app/Http/Controllers/Api/ProductController.php
@@ -45,19 +45,37 @@ namespace App\Http\Controllers\Api {
         public function store(Request $request)
         {
             //dd($request->all());
-            // Aqui poderia fazer verificacoes das informacoes que vem do resquest depois aprendo fazer isso
+            // Aqui poderia fazer verificações das informações que vem do request depois aprendo fazer isso
             try{
                 $productData = $request->all();
                 $this->product->create($productData);
-                return response()->json(['msg' => 'Produto criado com sucesso!'],201);
+                $datamsg = ['data' => ['msg' => 'Produto Criado com sucesso !']];
+                return response()->json($datamsg,201);
             } catch(\Exception $e){
                 if(config('app.debug')){
                     return response()->json(ApiError::errorMessage($e->getMessage(), 1010));
                 }
                     return response()->json(ApiError::errorMessage('Houve error ao realizar a operação', 1010));
             }
+        }
 
+        public function update(Request $request, $id)
+        {
 
+            try {
+                $productData = $request->all();
+                $product = $this->product->find($id);
+                $product->update($productData);
+
+                $damasks = ['data' => ['msg' => 'Produto atualizado com sucesso!']];
+                return response()->json($damasks, 201);
+
+            } catch (\Exception $e) {
+                if(config('app.debug')){
+                    return response()->json(ApiError::errorMessage($e->getMessage(),1010));
+                }
+                    return response()->json(ApiError::errorMessage('Houve um erro ao realizar conexão', 1010));
+            }
         }
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -28,6 +28,7 @@ Route::namespace('Api')->name('api.')->group(function(){
         Route::get('/{id}','ProductController@show')->name('single_products');
 
         Route::post('/','ProductController@store')->name('store_products');
+        Route::put('/{id}', 'ProductController@update')->name('update_products');
     });
 });
 
